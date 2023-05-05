30c8d5b963ed3bc2b94f215aeda84fd5d8a8127b
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-05-05 19:04:44 -0400

    Adds more tables to audit log

--- src/api/db/sql/audit_log.sql
+++ src/api/db/sql/audit_log.sql
@@ -1,11 +1,4 @@
 DROP TABLE IF EXISTS audit_log;
-CREATE TABLE audit_log (
-    id INTEGER PRIMARY KEY,
-    action_type_id INTEGER,
-    user_id INTEGER,
-    created_at TEXT DEFAULT((DATETIME('now', 'localtime')))
-);
-
 
 DROP TABLE IF EXISTS action_types;
 CREATE TABLE action_types (
@@ -17,3 +10,37 @@ INSERT INTO action_types (name) VALUES('Login');
 INSERT INTO action_types (name) VALUES('Add review');
 INSERT INTO action_types (name) VALUES('Add job');
 INSERT INTO action_types (name) VALUES('Add helpful review vote');
+INSERT INTO action_types (name) VALUES('Added role to user');
+
+-- Each action type can have an associated table with meta info
+-- about the action. For example, audit_log_user has information
+-- about user actions
+DROP TABLE IF EXISTS audit_log_user;
+CREATE TABLE audit_log_user (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    action_type_id INTEGER,
+    note VARCHAR(255),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(action_type_id) REFERENCES action_types(id)
+);
+
+DROP TABLE IF EXISTS audit_log_job;
+CREATE TABLE audit_log_job (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    action_type_id INTEGER,
+    note VARCHAR(255),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(action_type_id) REFERENCES action_types(id)
+);
+
+DROP TABLE IF EXISTS audit_log_review;
+CREATE TABLE audit_log_reviews(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    action_type_id INTEGER,
+    note VARCHAR(255),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(action_type_id) REFERENCES action_types(id)
+);
